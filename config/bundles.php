07eb716bfc2510caa1fcaca17c0728b8c81feefd
Alexey Xiro Vasiljev <vasiljev.alexey@gmail.com>
2018-05-08 14:52:27 +0300

    Add twig bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
